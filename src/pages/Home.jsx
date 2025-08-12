f8cc510d65ed21d73b073ef02135e50bb0b854b0
angiegit09 <liardangelique@gmail.com>
2025-08-12 12:44:15 +0000

    modification competences page home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,29 +9,31 @@ export default function Home() {
       {/*Bloc 1*/}
       <section className="image-bg">
         <div className="container-fluid text-center">
-          <div className="row col-12 col-mb-8">
-            <h1>Bonjour, je suis John Doe</h1>
-            <h2>Développeur web full stack</h2>
-            <div className="text-center">
-              <button
-                type="button"
-                className="btn btn-danger my-3 col-4 col-mb-3 col-ms-1 btn btn-danger border-0 rounded my-3"
-                onClick={() => setShowModal(true)}
-              >
-                En savoir plus
-              </button>
-              {showModal && <MonProfil onClose={() => setShowModal(false)} />}
+          <div className="row">
+            <div className=" col-12 col-mb-8">
+              <h1>Bonjour, je suis John Doe</h1>
+              <h2>Développeur web full stack</h2>
+              <div className="text-center">
+                <button
+                  type="button"
+                  className="btn btn-danger my-3 col-4 col-mb-3 col-ms-1 btn btn-danger border-0 rounded my-3"
+                  onClick={() => setShowModal(true)}
+                >
+                  En savoir plus
+                </button>
+                {showModal && <MonProfil onClose={() => setShowModal(false)} />}
+              </div>
             </div>
           </div>
         </div>
-         {/*Bloc 2*/}
+        {/*Bloc 2*/}
       </section>
       <div className="container shadow my-5">
         <div className="row g-4 justify-content-center align-items-start">
-           {/*colonne 1 gauche*/}
+          {/*colonne 1 gauche*/}
           <section className="col-11 col-md-6 p-4 ">
             <h2>A propos</h2>
-            <div className="bg-primary" style={{ height: "4px" }}>
+            <div className="bg-primary mb-3" style={{ height: "4px" }}>
               <hr className="custom-hr" />
             </div>
             <img
@@ -63,7 +65,7 @@ export default function Home() {
           <section className="col-11 col-md-6 p-4 ">
             <h2 className="section-title text-uppercase mb-3">
               Mes compétences
-              <div className="bg-primary" style={{ height: "4px" }}>
+              <div className="bg-primary mb-3" style={{ height: "4px" }}>
                 <hr className="custom-hr" />
               </div>
             </h2>
@@ -77,8 +79,8 @@ export default function Home() {
               </div>
               <div
                 className="progress"
-                role="progressbar"
                 aria-label="HTML5"
+                role="progressbar"
                 aria-valuenow="90"
                 aria-valuemin="0"
                 aria-valuemax="100"
@@ -86,7 +88,7 @@ export default function Home() {
                 <div
                   className="progress-bar bg-danger"
                   style={{ width: "90%" }}
-                ></div>
+                />
               </div>
             </div>
 
@@ -98,8 +100,8 @@ export default function Home() {
               </div>
               <div
                 className="progress"
-                role="progressbar"
                 aria-label="CSS3"
+                role="progressbar"
                 aria-valuenow="80"
                 aria-valuemin="0"
                 aria-valuemax="100"
@@ -107,11 +109,11 @@ export default function Home() {
                 <div
                   className="progress-bar bg-info"
                   style={{ width: "80%" }}
-                ></div>
+                />
               </div>
             </div>
 
-            {/* JavaScript */}
+            {/* JS */}
             <div className="mb-3">
               <div className="d-flex justify-content-between small mb-1">
                 <span>JavaScript</span>
@@ -119,8 +121,8 @@ export default function Home() {
               </div>
               <div
                 className="progress"
-                role="progressbar"
                 aria-label="JavaScript"
+                role="progressbar"
                 aria-valuenow="70"
                 aria-valuemin="0"
                 aria-valuemax="100"
@@ -128,28 +130,7 @@ export default function Home() {
                 <div
                   className="progress-bar bg-warning"
                   style={{ width: "70%" }}
-                ></div>
-              </div>
-            </div>
-
-            {/* PHP */}
-            <div className="mb-3">
-              <div className="d-flex justify-content-between small mb-1">
-                <span>PHP</span>
-                <span>60%</span>
-              </div>
-              <div
-                className="progress"
-                role="progressbar"
-                aria-label="PHP"
-                aria-valuenow="60"
-                aria-valuemin="0"
-                aria-valuemax="100"
-              >
-                <div
-                  className="progress-bar bg-success"
-                  style={{ width: "60%" }}
-                ></div>
+                />
               </div>
             </div>
 
@@ -161,8 +142,8 @@ export default function Home() {
               </div>
               <div
                 className="progress"
-                role="progressbar"
                 aria-label="React"
+                role="progressbar"
                 aria-valuenow="50"
                 aria-valuemin="0"
                 aria-valuemax="100"
@@ -170,7 +151,7 @@ export default function Home() {
                 <div
                   className="progress-bar bg-primary"
                   style={{ width: "50%" }}
-                ></div>
+                />
               </div>
             </div>
           </section>
